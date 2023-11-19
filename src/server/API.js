34f56e61df2ef9e70962e3c5776e0393a62fd683
Danil Sharifullin <qweber2019@mail.ru>
2023-11-19 14:38:06 +0300

    testing interceptors

--- src/server/API.js
+++ src/server/API.js
@@ -18,9 +18,11 @@ api.interceptors.response.use(config => {
     console.log(res, res.response.status)
     if (res.response.status === 401) {
         try {
-            await api.post('refresh-access-token', {
+            const response = await api.post('refresh-access-token', {
                 access: localStorage.getItem('token'),
             })
+            console.log(response)
+            localStorage.setItem('token', response.data.access_token)
         } catch(err) {
             console.log(err)
         }
